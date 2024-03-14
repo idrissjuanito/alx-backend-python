7dc001bffc46f1a0d322323d56342e2ad5fd2159
idrissjuanito <idrissjuanito@gmail.com>
2024-03-14 18:37:30 +0100

    python type annotations typevar for generics 2

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 """ returns values of a dictionary """
 from typing import Any, TypeVar, Mapping, Union
-
-
 T = TypeVar("T")
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None]) -> Union[Any, T]:
+
+
+def safely_get_value(
+        dct: Mapping,
+        key: Any,
+        default: Union[T, None]) -> Union[Any, T]:
     """ returns values of a dictionary """
     if key in dct:
         return dct[key]
