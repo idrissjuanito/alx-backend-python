890794afda167f70fe803b906c496410c595bfaa
idrissjuanito <idrissjuanito@gmail.com>
2024-03-18 12:08:59 +0100

    python async programming task 1

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -2,10 +2,11 @@
 """ random coroutine function """
 import random
 import time
+import asyncio
 
 
-async def wait_random(max_delay: int = 10):
+async def wait_random(max_delay: int = 10) -> float:
     """ sleep for random sleep time """
     timee = random.uniform(0, max_delay)
-    time.sleep(timee)
+    await asyncio.sleep(timee)
     return timee
