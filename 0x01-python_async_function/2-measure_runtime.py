55e63d7672c3033dc13ffdc9175eeaa56aecfedb
idrissjuanito <idrissjuanito@gmail.com>
2024-03-20 00:44:29 +0100

    python async functions 2

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -3,14 +3,14 @@
 import time
 import asyncio
 from typing import List
-wait_random = __import__("0-basic_async_syntax.py").wait_random
+wait_n = __import__("1-concurrent_coroutines").wait_n
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def measure_time(n: int, max_delay: int) -> List[float]:
     """ run coroutine n times """
     completed = list()
     for i in range(0, n):
-        time_task = await wait_random(max_delay)
+        time_task = await wait_n(n, max_delay)
         if len(completed) < 1:
             completed.append(time_task)
             continue
