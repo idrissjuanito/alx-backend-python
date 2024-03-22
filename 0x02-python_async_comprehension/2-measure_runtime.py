f1c9da55a05e7d89802c6b521cc3c85f9b3e9027
idrissjuanito <idrissjuanito@gmail.com>
2024-03-22 19:09:51 +0100

    async comprehension task 03

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -5,9 +5,9 @@ import asyncio
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-def measure_runtime():
+async def measure_runtime():
     """ measures the total time taken to run number of async coroutines """
     start = time.perf_counter()
-    asyncio.gather(*[async_comprehension() for i in range(4)])
+    results = await asyncio.gather(*[async_comprehension() for i in range(4)])
     total_time = time.perf_counter() - start
     return total_time
