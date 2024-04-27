d6af46255a95ca7bac48aff750b2520418fc71f5
idrissjuanito <idrissjuanito@gmail.com>
2024-04-27 02:18:44 +0000

    Flast i18 3

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" Client Testing """
+from unittest import TestCase
+from unittest.mock import patch
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(TestCase):
+    """ Testing http client """
+    @parameterized.expand([
+            ("google",),
+            ("abc",)
+        ])
+    @patch('client.get_json')
+    def test_org(self, org, json_mock):
+        """ testing org method """
+        json_mock.return_value = None
+        inst = GithubOrgClient(org)
+        inst.org
+        req_url = inst.ORG_URL.format(org=org)
+        json_mock.assert_called_once_with(req_url)
