e5fc92421421a473c27618edd4d1accbdcc298ab
idrissjuanito <idrissjuanito@gmail.com>
2024-04-27 02:42:59 +0000

    Flast i18 3

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -20,3 +20,11 @@ class TestGithubOrgClient(TestCase):
         inst.org
         req_url = inst.ORG_URL.format(org=org)
         json_mock.assert_called_once_with(req_url)
+
+    def test_public_repos_url(self):
+        """ test puvli url """
+        with patch.object(GithubOrgClient, 'org') as org_mock:
+            org_mock.return_value = {'repos_url': 'makeint'}
+            new_test = GithubOrgClient("maker")
+            new_test.org
+            self.assertEqual(new_test._public_repos_url, "makeint")
